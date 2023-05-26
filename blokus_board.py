6ff4323b15a4c9cb1013051f773e93ef273df980
Erin Shang <erin.shang.yiran@gmail.com>
2023-05-26 16:18:55 -0700

    Blokus board layout

---
+++ blokus_board.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+for i in range(29):
+    print("--",end="")
+for i in range(14):
+    print()
+    for j in range(14):
+        print("|   ", end="")
+    print("|", end="")
+    print()
+    for i in range(29):
+        print("--", end="")
